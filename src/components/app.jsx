9218f3204c1a56662fa63a252168b46a721b27d7
Yuta <a.yuta.0401@gmnail.com>
2019-04-28 21:09:02 +0900

    auto format

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Greeting from './greeting';
+import Greeting from "./greeting";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name : "KJ",
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "KJ"
+    };
+  }
+  handleChangeName(name) {
+    this.setState({name});
+  }
 
-    handleMouseOver() {
-        this.setState({ name:"Tom"});
-    }
-
-    handleMouseOut() {
-        this.setState({ name:"James"});
-    }
-
-    render () {
-        return (
-        <div
-        onMouseOver = {() => this.handleMouseOver()}
-        onMouseOut={() => this.handleMouseOut()}
-        >
-            <Greeting name={this.state.name}/>
-        </div>);
-    }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleChangeName(e.target.value)}
+            />
+            <button onClick={() => this.handleChangeName("Bob")}>I am Geroge</button>
+          <Greeting name={this.state.name} />
+      </div>
+    );
+  }
 }
 export default App;
