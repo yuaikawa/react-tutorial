858b6d522d8ca187ee7e35d4e262f4ff62732870
Yuta <a.yuta.0401@gmnail.com>
2019-04-28 11:38:18 +0900

    無名関数を呼んで遊んでみた

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : "KJ",
+        }
+    }
+
+    handleMouseOver() {
+        this.setState({ name:"Tom"});
+    }
+
+    handleMouseOut() {
+        this.setState({ name:"James"});
+    }
+
     render () {
         return (
-        <div>
-            <Greeting name="john"/>
-            <Greeting name="tanaka"/>
+        <div
+        onMouseOver = {() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+        >
+            <Greeting name={this.state.name}/>
         </div>);
     }
 }
