03fb9c99b2326a4f23cab45ddcf1c15384c96cd7
Yuta <a.yuta.0401@gmnail.com>
2019-04-29 23:41:28 +0900

    scssの調整

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component,PropTypes } from "react";
+import { className } from "postcss-selector-parser";
 
 class SearchForm extends Component {
   constructor(props) {
@@ -19,13 +20,15 @@ class SearchForm extends Component {
 
   render() {
     return (
-      <form onSubmit={e =>this.handleSubmit(e)}>
+      <form className="search-form" onSubmit={e =>this.handleSubmit(e)}>
         <input
+          className="place-input"
           type="text"
+          size="30"
           value={this.state.place}
           onChange={e => this.handlePlaceChange(e.target.value)}
         />
-        <input type="submit" value="検索"/>
+        <input className="submit-button" type="submit" value="検索"/>
       </form>
     );
   }
